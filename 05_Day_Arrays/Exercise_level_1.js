0cf38e90560410ddd801b54e378118c34157b8a4
narayan2111 <narayandwivedi076gmail.com>
2023-08-28 21:04:15 +0530

    Day 5

---
+++ 05_Day_Arrays/Exercise_level_1.js
@@ -0,0 +1,23 @@
+// const arr = Array();
+// console.log(arr);
+// const arr1 = [];
+// console.log(arr1);
+
+// const arr = [1,2,3,4,5,6];
+// console.log(`Array:`,arr);
+
+// console.log(`Array Size :`,arr.length);
+// let startIndex = 0;
+// console.log(`Array first Element:`,arr[0]);
+
+// var lastIndex = arr.length-1;
+// var mid = parseInt((lastIndex+startIndex)/2);
+// console.log(`Array middle element:`, arr[mid]);
+// console.log(`Array last element:`,arr[lastIndex]);
+
+//5. Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+
+const arr = [{Name:'Narayan'},{course:`MCA`},{Age:23},{Session:`2023-24`},{City:`Greater Noida`},{College_Name:`GCET, Greater Noida`}];
+console.log(arr.length);
+console.log(arr);
+console.log(arr[0]);
---
+++ 05_Day_Arrays/SampleArray.js
@@ -0,0 +1,6 @@
+const arr = [{Name: `Narayan`},{Course:`MCA`},{city:`Prayagraj`}];
+
+//console.log(arr);
+
+console.log(arr[0]);
+console.log(arr.length);
